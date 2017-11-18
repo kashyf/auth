b71fe3296aec0312bb224d3fed5dfbb3cd387e02
Jawwad Zafar <zafarjawwad@gmail.com>
2017-11-19 01:32:06 +0530

    auth

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,9 +1,20 @@
-(function ($, window, document, undefined) {
-
-  'use strict';
-
-  $(function () {
-    // WRITE YOUR JQUERY HERE
+/*!
+ * Authorito
+ * Authorito
+ * 
+ * @author Authorito
+ * @version 1.0.5
+ * Copyright 2017. MIT licensed.
+ */
+$(function() {
+  var header = $('.navbar-up');
+  console.log(header);
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 10) {
+      header.removeClass('navbar-up').addClass('navbar-down');
+    } else {
+      header.removeClass('navbar-down').addClass('navbar-up');
+    }
   });
-
-})(jQuery, window, document);
+});
