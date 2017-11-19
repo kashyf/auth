bc2d8b1c252b340237a0c6d7a0d1503a74102469
Jawwad Zafar <zafarjawwad@gmail.com>
2017-11-20 04:21:16 +0530

    branch

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,9 +6,29 @@
  * @version 1.0.5
  * Copyright 2017. MIT licensed.
  */
-$(function() {
+// scroll smoother
+$('a.navtext').click(function() {
+  $('html, body').animate({
+    scrollTop: $($(this).attr('href')).offset().top - 80
+  }, 1000);
+  return false;
+});
+
+// team overlay
+$(".member .overlay").hover(function() {
+  $(this).addClass("active")
+}, function() {
+  $(this).removeClass("active")
+});
+$(".member .overlay").on("tap", function() {
+  $(this).addClass("active")
+});
+$(".member .img").on("tap", function() {
+  $(this).addClass("active")
+});
+
+$(document).ready(function() {
   var header = $('.navbar-up');
-  console.log(header);
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll >= 10) {
@@ -16,5 +36,12 @@ $(function() {
     } else {
       header.removeClass('navbar-down').addClass('navbar-up');
     }
+  });
+  var url = $("#authvideo").attr('src');
+  $("#modal-video").on('hide.bs.modal', function() {
+    $("#authvideo").attr('src', '');
+  });
+  $("#modal-video").on('show.bs.modal', function() {
+    $("#authvideo").attr('src', url);
   });
 });
